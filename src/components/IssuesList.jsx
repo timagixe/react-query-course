b15c4e1293da0d0a6550e0948acebc881a841d01
Tima Gixe <timagixe@protonmail.com>
2022-06-11 14:54:04 +0300

    fetch issues list

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,36 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import IssueItem from "./IssueItem";
+
+async function fetchIssues() {
+  const url = "api/issues";
+  const data = await fetch(url);
+  return await data.json();
+}
 
 export default function IssuesList() {
+  const issuesQuery = useQuery(["issues"], fetchIssues);
+
   return (
     <div>
       <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      {issuesQuery.isLoading && <p>Loading...</p>}
+      {issuesQuery.isSuccess && (
+        <ul className="issues-list">
+          {issuesQuery.data.map((issue) => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              assignee={issue.assignee}
+              commentCount={issue.comments.length}
+              createdBy={issue.createdBy}
+              createdDate={issue.createdDate}
+              labels={issue.labels}
+              status={issue.status}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
