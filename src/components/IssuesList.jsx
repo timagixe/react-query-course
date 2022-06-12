b19809f51cbb4e078e134f35e381d0b8511dfd54
Tima Gixe <timagixe@protonmail.com>
2022-06-12 19:08:32 +0300

    adjust issues list component

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,6 +1,8 @@
 import useIssuesQuery from "../hooks/useIssuesQuery";
 import IssueItem from "./IssueItem";
-import { memo } from "react";
+import { memo, useState, useCallback } from "react";
+import useSearchIssuesQuery from "../hooks/useSearchIssuesQuery";
+import isIdleLoading from "../helpers/isIdleLoading";
 
 function IssuesList({ selectedLabels, selectedStatus }) {
   const issuesQuery = useIssuesQuery({
@@ -8,30 +10,80 @@ function IssuesList({ selectedLabels, selectedStatus }) {
     status: selectedStatus,
   });
 
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchIssuesQuery = useSearchIssuesQuery(searchValue);
+
+  const onSubmit = useCallback((event) => {
+    event.preventDefault();
+    setSearchValue(event.target.elements.search.value);
+  }, []);
+
+  const onSearchChange = useCallback(
+    (event) => {
+      if (!event.target.value.length) {
+        setSearchValue("");
+      }
+    },
+    [setSearchValue]
+  );
+
   return (
     <div>
-      <h2>Issues List</h2>
-      {issuesQuery.isLoading && <p>Loading...</p>}
-      {issuesQuery.isSuccess && (
+      <form onSubmit={onSubmit}>
+        <label htmlFor="search">Search Issues</label>
+        <input
+          type="search"
+          placeholder="Search"
+          name="search"
+          id="search"
+          onChange={onSearchChange}
+        />
+      </form>
+      <h2>{Boolean(searchValue) ? "Search Results" : "Issues List"}</h2>
+      {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : isIdleLoading(searchIssuesQuery) ? (
         <>
-          {Boolean(issuesQuery.data.length) ? (
-            <ul className="issues-list">
-              {issuesQuery.data.map((issue) => (
-                <IssueItem
-                  key={issue.id}
-                  title={issue.title}
-                  number={issue.number}
-                  assignee={issue.assignee}
-                  commentCount={issue.comments.length}
-                  createdBy={issue.createdBy}
-                  createdDate={issue.createdDate}
-                  labels={issue.labels}
-                  status={issue.status}
-                />
-              ))}
-            </ul>
+          <ul className="issues-list">
+            {issuesQuery.data.map((issue) => (
+              <IssueItem
+                key={issue.id}
+                title={issue.title}
+                number={issue.number}
+                assignee={issue.assignee}
+                commentCount={issue.comments.length}
+                createdBy={issue.createdBy}
+                createdDate={issue.createdDate}
+                labels={issue.labels}
+                status={issue.status}
+              />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <>
+          {searchIssuesQuery.isLoading ? (
+            <p>Loading...</p>
           ) : (
-            <p>No issues found.</p>
+            <>
+              <p>{searchIssuesQuery.data.count} Results</p>
+              <ul className="issues-list">
+                {searchIssuesQuery.data.items.map((issue) => (
+                  <IssueItem
+                    key={issue.id}
+                    title={issue.title}
+                    number={issue.number}
+                    assignee={issue.assignee}
+                    commentCount={issue.comments.length}
+                    createdBy={issue.createdBy}
+                    createdDate={issue.createdDate}
+                    labels={issue.labels}
+                    status={issue.status}
+                  />
+                ))}
+              </ul>
+            </>
           )}
         </>
       )}
