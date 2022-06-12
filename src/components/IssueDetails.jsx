042ae59a69449c494684d319577493a69061710c
Tima Gixe <timagixe@protonmail.com>
2022-06-12 11:35:29 +0300

    adjust issue details component

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,7 +1,51 @@
 import { useParams } from "react-router-dom";
+import useIssueCommentsQuery from "../hooks/useCommentsQuery";
+import useIssueQuery from "../hooks/useIssueQuery";
+import IssueHeader from "./IssueHeader";
+import Comment from "./Comment";
+import { memo } from "react";
 
-export default function IssueDetails() {
+function IssueDetails() {
   const { number } = useParams();
+  const issueQuery = useIssueQuery(number);
+  const commentsQuery = useIssueCommentsQuery(number);
 
-  return <h1>Issue {number}</h1>;
+  return (
+    <div className="issue-details">
+      {issueQuery.isLoading ? (
+        <p>Loading issue...</p>
+      ) : (
+        <>
+          <IssueHeader
+            title={issueQuery.data.title}
+            number={issueQuery.data.number}
+            status={issueQuery.data.status}
+            createdBy={issueQuery.data.createdBy}
+            createdDate={issueQuery.data.createdDate}
+            comments={issueQuery.data.comments}
+          />
+
+          <main>
+            <section>
+              {commentsQuery.isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                commentsQuery.data?.map((comment) => (
+                  <Comment
+                    key={comment.id}
+                    comment={comment.comment}
+                    createdBy={comment.createdBy}
+                    createdDate={comment.createdDate}
+                  />
+                ))
+              )}
+            </section>
+            <aside></aside>
+          </main>
+        </>
+      )}
+    </div>
+  );
 }
+
+export default memo(IssueDetails);
