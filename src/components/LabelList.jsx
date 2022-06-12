cf73bbd3062c5184b62d80185102f0798e94565e
Tima Gixe <timagixe@protonmail.com>
2022-06-12 10:25:42 +0300

    update label list component

--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,33 @@
-export default function LabelList() {
-  return <h3>Labels</h3>;
+import useLabelsQuery from "../hooks/useLabelsQuery";
+import { memo } from "react";
+
+function LabelList({ selectedLabels, onToggleLabel }) {
+  const labelsQuery = useLabelsQuery();
+  return (
+    <div className="labels">
+      <h3>Labels</h3>
+      {labelsQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {labelsQuery.data.map((label) => {
+            return (
+              <li key={label.id}>
+                <button
+                  onClick={() => onToggleLabel(label.id)}
+                  className={`label ${
+                    selectedLabels.includes(label.id) ? "selected " : ""
+                  }${label.color}`}
+                >
+                  {label.name}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
+
+export default memo(LabelList);
