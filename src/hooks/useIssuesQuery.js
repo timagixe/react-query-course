69b821777e5becfcfcaa3ddc3da73f23e8d77e0b
Tima Gixe <timagixe@protonmail.com>
2022-06-12 10:26:05 +0300

    update use issues query hook

--- src/hooks/useIssuesQuery.js
+++ src/hooks/useIssuesQuery.js
@@ -1,14 +1,21 @@
 import { useQuery } from "react-query";
 import handleResponse from "../helpers/handleResponse";
 
-const issuesUrl = () => "/api/issues";
+const issuesUrl = (labels) => {
+  const hasLabels = Boolean(labels.length);
+  const labelsQueryParams = hasLabels
+    ? labels.map((label) => `labels[]=${label}`).join("&")
+    : "";
+  return `/api/issues?${labelsQueryParams}`;
+};
 
-function queryIssuesFunction() {
-  return fetch(issuesUrl()).then(
+function queryIssuesFunction({ queryKey: [{ labels }] }) {
+  return fetch(issuesUrl(labels)).then(
     handleResponse({ onErrorMessage: "Couldn't load issues list." })
   );
 }
 
-export default function useIssuesQuery() {
-  return useQuery([{ scope: "issues" }], queryIssuesFunction);
+export default function useIssuesQuery(labels) {
+  console.log("TEST");
+  return useQuery([{ scope: "issues", labels }], queryIssuesFunction);
 }
