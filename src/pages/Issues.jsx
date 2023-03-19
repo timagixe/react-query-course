b6c3c5693cafad8275f12f1edf499c047f1d95a3
Tima Gixe <timagixe@protonmail.com>
2023-03-19 21:03:53 +0200

    add Add Issue button

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,5 +1,6 @@
 import { useState, useCallback } from "react";
 import { memo } from "react";
+import { Link } from "react-router-dom";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import StatusSelect from "../components/StatusSelect";
@@ -32,6 +33,10 @@ function Issues() {
         <aside>
           <LabelList selectedLabels={labels} onToggleLabel={onToggleLabel} />
           <StatusSelect value={status} onChange={onStatusChange} />
+          <hr />
+          <Link className="button" to="/add">
+            Add Issue
+          </Link>
         </aside>
       </main>
     </div>
