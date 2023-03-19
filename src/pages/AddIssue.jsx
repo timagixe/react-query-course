5bd5500be77317eb10772afd561c81a2b892bdf6
Tima Gixe <timagixe@protonmail.com>
2023-03-19 21:04:13 +0200

    update Add Issue page

--- src/pages/AddIssue.jsx
+++ src/pages/AddIssue.jsx
@@ -1,7 +1,46 @@
 import { memo } from "react";
+import { useNavigate } from "react-router-dom";
+import useAddIssueMutation from "../hooks/useAddIssueMutation";
 
 function AddIssue() {
-  return <h2>Add Issue</h2>;
+    const navigate = useNavigate();
+    const addIssueMutation = useAddIssueMutation({
+        onSuccess: (data) => navigate(`/issue/${data.number}`)
+    });
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (addIssueMutation.isLoading) return;
+        addIssueMutation.mutate({
+            title: event.target.title.value,
+            comment: event.target.comment.value
+        });
+    };
+    return (
+        <div className="add-issue">
+            <h2>Add Issue</h2>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="title">Title</label>
+                <input
+                    type="text"
+                    id="title"
+                    placeholder="Title"
+                    name="title"
+                />
+                <label htmlFor="comment">Comment</label>
+                <textarea
+                    placeholder="Comment"
+                    id="comment"
+                    name="comment"
+                />
+                <button
+                    type="submit"
+                    disabled={addIssueMutation.isLoading}
+                >
+                    {addIssueMutation.isLoading ? "Adding Issue..." : "Add Issue"}
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default memo(AddIssue);
