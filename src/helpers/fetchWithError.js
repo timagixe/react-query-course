540ad05de2dad21d6f3321729bcb3b92d95c42dc
Tima Gixe <timagixe@protonmail.com>
2023-03-19 21:03:36 +0200

    update fetchWithError

--- src/helpers/fetchWithError.js
+++ src/helpers/fetchWithError.js
@@ -1,19 +1,14 @@
 export default async function fetchWithError(url, options) {
-  try {
-    const response = await fetch(url, options);
-
-    if (response.status === 200) {
-      const data = await response.json();
-
-      if ("error" in data) {
-        throw new Error(data.error);
-      }
-
-      return data;
+    try {
+        const response = await fetch(url, options);
+        const isOkResponse = response.status >= 200 && response.status < 300;
+        if (!isOkResponse) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        if ("error" in data) throw new Error(data.error);
+        return data;
+    } catch (error) {
+        throw new Error(error.message);
     }
-
-    throw new Error(`Error ${response.status}: ${response.statusText}`);
-  } catch (error) {
-    throw new Error(error.message);
-  }
 }
