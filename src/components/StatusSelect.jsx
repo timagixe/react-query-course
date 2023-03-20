77c8d4f97be352e0ce195879cf8dcb2a594c2950
Tima Gixe <timagixe@protonmail.com>
2023-03-20 09:35:32 +0200

    update StatusSelect component

--- src/components/StatusSelect.jsx
+++ src/components/StatusSelect.jsx
@@ -1,17 +1,26 @@
 import { memo } from "react";
 import { STATUSES } from "../helpers/defaultData";
 
-function StatusSelect({ value, onChange }) {
-  return (
-    <select value={value} onChange={onChange} className="status-select">
-      <option value="">Select a status to filter</option>
-      {STATUSES.map(({ id, label }) => (
-        <option value={id} key={id}>
-          {label}
-        </option>
-      ))}
-    </select>
-  );
+function StatusSelect({ value, onChange, noEmptyOption = false }) {
+    return (
+        <select
+            value={value}
+            onChange={onChange}
+            className="status-select"
+        >
+            {!noEmptyOption && (
+                <option value="">Select a status to filter</option>
+            )}
+            {STATUSES.map(({ id, label }) => (
+                <option
+                    value={id}
+                    key={id}
+                >
+                    {label}
+                </option>
+            ))}
+        </select>
+    );
 }
 
 export default memo(StatusSelect);
