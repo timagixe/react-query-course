4a41b8fbf1802a9fc090dc58aa7492a802ceb135
Tima Gixe <timagixe@protonmail.com>
2023-03-20 09:35:52 +0200

    update IssueDetails component

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -4,48 +4,54 @@ import useIssueQuery from "../hooks/useIssueQuery";
 import IssueHeader from "./IssueHeader";
 import Comment from "./Comment";
 import { memo } from "react";
+import IssueStatus from "./IssueStatus";
 
 function IssueDetails() {
-  const { number } = useParams();
-  const issueQuery = useIssueQuery(number);
-  const commentsQuery = useIssueCommentsQuery(number);
+    const { number } = useParams();
+    const issueQuery = useIssueQuery(number);
+    const commentsQuery = useIssueCommentsQuery(number);
 
-  return (
-    <div className="issue-details">
-      {issueQuery.isLoading ? (
-        <p>Loading issue...</p>
-      ) : (
-        <>
-          <IssueHeader
-            title={issueQuery.data.title}
-            number={issueQuery.data.number}
-            status={issueQuery.data.status}
-            createdBy={issueQuery.data.createdBy}
-            createdDate={issueQuery.data.createdDate}
-            comments={issueQuery.data.comments}
-          />
+    return (
+        <div className="issue-details">
+            {issueQuery.isLoading ? (
+                <p>Loading issue...</p>
+            ) : (
+                <>
+                    <IssueHeader
+                        title={issueQuery.data.title}
+                        number={issueQuery.data.number}
+                        status={issueQuery.data.status}
+                        createdBy={issueQuery.data.createdBy}
+                        createdDate={issueQuery.data.createdDate}
+                        comments={issueQuery.data.comments}
+                    />
 
-          <main>
-            <section>
-              {commentsQuery.isLoading ? (
-                <p>Loading...</p>
-              ) : (
-                commentsQuery.data?.map((comment) => (
-                  <Comment
-                    key={comment.id}
-                    comment={comment.comment}
-                    createdBy={comment.createdBy}
-                    createdDate={comment.createdDate}
-                  />
-                ))
-              )}
-            </section>
-            <aside></aside>
-          </main>
-        </>
-      )}
-    </div>
-  );
+                    <main>
+                        <section>
+                            {commentsQuery.isLoading ? (
+                                <p>Loading...</p>
+                            ) : (
+                                commentsQuery.data?.map((comment) => (
+                                    <Comment
+                                        key={comment.id}
+                                        comment={comment.comment}
+                                        createdBy={comment.createdBy}
+                                        createdDate={comment.createdDate}
+                                    />
+                                ))
+                            )}
+                        </section>
+                        <aside>
+                            <IssueStatus
+                                issueNumber={issueQuery.data.number.toString()}
+                                status={issueQuery.data.status}
+                            />
+                        </aside>
+                    </main>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default memo(IssueDetails);
