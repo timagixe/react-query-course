2b577290378ae359065b17cfdc08a57e3db5e8f4
Tima Gixe <timagixe@protonmail.com>
2023-06-17 13:27:54 +0300

    extend issues list with pagination

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -5,93 +5,123 @@ import useSearchIssuesQuery from "../hooks/useSearchIssuesQuery";
 import isIdleLoading from "../helpers/isIdleLoading";
 import Loader from "./Loader";
 
-function IssuesList({ selectedLabels, selectedStatus }) {
-  const issuesQuery = useIssuesQuery({
-    labels: selectedLabels,
-    status: selectedStatus,
-  });
+function IssuesList({ selectedLabels, selectedStatus, pageNumber, onPageChange }) {
+    const issuesQuery = useIssuesQuery({
+        labels: selectedLabels,
+        status: selectedStatus,
+        pageNumber
+    });
 
-  const [searchValue, setSearchValue] = useState("");
+    const [searchValue, setSearchValue] = useState("");
 
-  const searchIssuesQuery = useSearchIssuesQuery(searchValue);
+    const searchIssuesQuery = useSearchIssuesQuery(searchValue);
 
-  const onSubmit = useCallback((event) => {
-    event.preventDefault();
-    setSearchValue(event.target.elements.search.value);
-  }, []);
+    const onSubmit = useCallback((event) => {
+        event.preventDefault();
+        setSearchValue(event.target.elements.search.value);
+    }, []);
 
-  const onSearchChange = useCallback(
-    (event) => {
-      if (!event.target.value.length) {
-        setSearchValue("");
-      }
-    },
-    [setSearchValue]
-  );
+    const onSearchChange = useCallback(
+        (event) => {
+            if (!event.target.value.length) {
+                setSearchValue("");
+            }
+        },
+        [setSearchValue]
+    );
 
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="search">Search Issues</label>
-        <input
-          type="search"
-          placeholder="Search"
-          name="search"
-          id="search"
-          onChange={onSearchChange}
-        />
-      </form>
-      <h2>{Boolean(searchValue) ? "Search Results" : ["Issues List", issuesQuery.isFetching && <Loader />]}</h2>
-      {issuesQuery.isLoading ? (
-        <p>Loading...</p>
-      ) : issuesQuery.isError ? (
-        <p>{issuesQuery.error.message}</p>
-      ) : isIdleLoading(searchIssuesQuery) ? (
-        <>
-          <ul className="issues-list">
-            {issuesQuery.data.map((issue) => (
-              <IssueItem
-                key={issue.id}
-                title={issue.title}
-                number={issue.number}
-                assignee={issue.assignee}
-                commentCount={issue.comments.length}
-                createdBy={issue.createdBy}
-                createdDate={issue.createdDate}
-                labels={issue.labels}
-                status={issue.status}
-              />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <>
-          {searchIssuesQuery.isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            <>
-              <p>{searchIssuesQuery.data.count} Results</p>
-              <ul className="issues-list">
-                {searchIssuesQuery.data.items.map((issue) => (
-                  <IssueItem
-                    key={issue.id}
-                    title={issue.title}
-                    number={issue.number}
-                    assignee={issue.assignee}
-                    commentCount={issue.comments.length}
-                    createdBy={issue.createdBy}
-                    createdDate={issue.createdDate}
-                    labels={issue.labels}
-                    status={issue.status}
-                  />
-                ))}
-              </ul>
-            </>
-          )}
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="search">Search Issues</label>
+                <input
+                    type="search"
+                    placeholder="Search"
+                    name="search"
+                    id="search"
+                    onChange={onSearchChange}
+                />
+            </form>
+            <h2>
+                {Boolean(searchValue)
+                    ? "Search Results"
+                    : ["Issues List", issuesQuery.isFetching && <Loader />]}
+            </h2>
+            {issuesQuery.isLoading ? (
+                <p>Loading...</p>
+            ) : issuesQuery.isError ? (
+                <p>{issuesQuery.error.message}</p>
+            ) : isIdleLoading(searchIssuesQuery) ? (
+                <>
+                    <ul className="issues-list">
+                        {issuesQuery.data.map((issue) => (
+                            <IssueItem
+                                key={issue.id}
+                                title={issue.title}
+                                number={issue.number}
+                                assignee={issue.assignee}
+                                commentCount={issue.comments.length}
+                                createdBy={issue.createdBy}
+                                createdDate={issue.createdDate}
+                                labels={issue.labels}
+                                status={issue.status}
+                            />
+                        ))}
+                    </ul>
+                    <div className="pagination">
+                        <button
+                            onClick={() => {
+                                if (pageNumber - 1 > 0) {
+                                    onPageChange(pageNumber - 1);
+                                }
+                            }}
+                            disabled={pageNumber === 1}
+                        >
+                            Previous
+                        </button>
+                        <p>
+                            Page {pageNumber} {issuesQuery.isFetching ? "..." : ""}
+                        </p>
+                        <button
+                            disabled={issuesQuery.data?.length === 0 || issuesQuery.isPreviousData}
+                            onClick={() => {
+                                if (issuesQuery.data?.length !== 0 && !issuesQuery.isPreviousData) {
+                                    onPageChange(pageNumber + 1);
+                                }
+                            }}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    {searchIssuesQuery.isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <>
+                            <p>{searchIssuesQuery.data.count} Results</p>
+                            <ul className="issues-list">
+                                {searchIssuesQuery.data.items.map((issue) => (
+                                    <IssueItem
+                                        key={issue.id}
+                                        title={issue.title}
+                                        number={issue.number}
+                                        assignee={issue.assignee}
+                                        commentCount={issue.comments.length}
+                                        createdBy={issue.createdBy}
+                                        createdDate={issue.createdDate}
+                                        labels={issue.labels}
+                                        status={issue.status}
+                                    />
+                                ))}
+                            </ul>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
 }
 
 export default memo(IssuesList);
