76080ddc4f1d63d75ebc48bf4bb2db711696b4f2
Tima Gixe <timagixe@protonmail.com>
2023-06-17 13:27:45 +0300

    setup page number state

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -6,41 +6,57 @@ import LabelList from "../components/LabelList";
 import StatusSelect from "../components/StatusSelect";
 
 function Issues() {
-  const [labels, setLabels] = useState([]);
-  const [status, setStatus] = useState("");
+    const [labels, setLabels] = useState([]);
+    const [status, setStatus] = useState("");
+    const [pageNumber, setPageNumber] = useState(1);
 
-  const onToggleLabel = useCallback((labelId) => {
-    setLabels((old) => {
-      if (old.includes(labelId)) {
-        return old.filter((id) => id !== labelId);
-      }
-      return old.concat(labelId);
-    });
-  }, []);
+    const onToggleLabel = useCallback((labelId) => {
+        setPageNumber(1);
+        setLabels((old) => {
+            if (old.includes(labelId)) {
+                return old.filter((id) => id !== labelId);
+            }
+            return old.concat(labelId);
+        });
+    }, []);
 
-  const onStatusChange = useCallback(
-    ({ target: { value } }) => setStatus(value),
-    []
-  );
+    const onStatusChange = useCallback(({ target: { value } }) => {
+        setPageNumber(1);
+        setStatus(value);
+    }, []);
 
-  return (
-    <div>
-      <main>
-        <section>
-          <h1>Issues</h1>
-          <IssuesList selectedLabels={labels} selectedStatus={status} />
-        </section>
-        <aside>
-          <LabelList selectedLabels={labels} onToggleLabel={onToggleLabel} />
-          <StatusSelect value={status} onChange={onStatusChange} />
-          <hr />
-          <Link className="button" to="/add">
-            Add Issue
-          </Link>
-        </aside>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <main>
+                <section>
+                    <h1>Issues</h1>
+                    <IssuesList
+                        selectedLabels={labels}
+                        selectedStatus={status}
+                        pageNumber={pageNumber}
+                        onPageChange={setPageNumber}
+                    />
+                </section>
+                <aside>
+                    <LabelList
+                        selectedLabels={labels}
+                        onToggleLabel={onToggleLabel}
+                    />
+                    <StatusSelect
+                        value={status}
+                        onChange={onStatusChange}
+                    />
+                    <hr />
+                    <Link
+                        className="button"
+                        to="/add"
+                    >
+                        Add Issue
+                    </Link>
+                </aside>
+            </main>
+        </div>
+    );
 }
 
 export default memo(Issues);
