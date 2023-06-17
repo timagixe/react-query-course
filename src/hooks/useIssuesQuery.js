87fe5fd39216a91b7cf798d7fe1d773a618e23ac
Tima Gixe <timagixe@protonmail.com>
2023-06-17 13:27:33 +0300

    pass page number to issues query

--- src/hooks/useIssuesQuery.js
+++ src/hooks/useIssuesQuery.js
@@ -2,37 +2,39 @@ import { useQuery, useQueryClient } from "react-query";
 import fetchWithError from "../helpers/fetchWithError";
 import { STALE_TIME } from "../helpers/staleTime";
 
-const issuesUrl = ({ labels, status }) => {
-  const hasLabels = Boolean(labels.length);
-  const statusQueryParam = status ? `&status=${status}` : "";
-  const labelsQueryParam = hasLabels
-    ? labels.map((label) => `labels[]=${label}`).join("&")
-    : "";
-  return `/api/issues?${labelsQueryParam}${statusQueryParam}`;
+const issuesUrl = ({ labels, status, pageNumber }) => {
+    const hasLabels = Boolean(labels.length);
+    const statusQueryParam = status ? `status=${status}` : "";
+    const labelsQueryParam = hasLabels ? labels.map((label) => `labels[]=${label}`).join("&") : "";
+    const pageQueryParam = pageNumber ? `page=${pageNumber}` : "";
+    const params = [labelsQueryParam, statusQueryParam, pageQueryParam].filter(Boolean).join("&");
+    return `/api/issues?${params}`;
 };
 
-function queryIssuesFunction({ queryKey: [{ labels, status }], signal }, queryClient) {
-  return fetchWithError(issuesUrl({ labels, status }), { signal }).then(issues => {
-    if (Array.isArray(issues)) {
-      for (const issue of issues) {
-        queryClient.setQueryData(
-          [{
-            scope: "issue",
-            number: String(issue.number)
-          }],
-          issue
-        );
-      }
-    }
-    return issues;
-  });
+function queryIssuesFunction({ queryKey: [{ labels, status, pageNumber }], signal }, queryClient) {
+    return fetchWithError(issuesUrl({ labels, status, pageNumber }), { signal }).then((issues) => {
+        if (Array.isArray(issues)) {
+            for (const issue of issues) {
+                queryClient.setQueryData(
+                    [
+                        {
+                            scope: "issue",
+                            number: String(issue.number)
+                        }
+                    ],
+                    issue
+                );
+            }
+        }
+        return issues;
+    });
 }
 
-export default function useIssuesQuery({ labels, status }) {
-  const queryClient = useQueryClient();
-  return useQuery(
-    [{ scope: "issues", labels, status }],
-    (context) => queryIssuesFunction(context, queryClient),
-    { staleTime: STALE_TIME.ONE_MINUTE }
-  );
+export default function useIssuesQuery({ labels, status, pageNumber }) {
+    const queryClient = useQueryClient();
+    return useQuery(
+        [{ scope: "issues", labels, status, pageNumber }],
+        (context) => queryIssuesFunction(context, queryClient),
+        { staleTime: STALE_TIME.ONE_MINUTE, keepPreviousData: true }
+    );
 }
